8981b75a1b5a6cc0a04dea3b7de7fb7257b11990
chetan <chetanpt@users.noreply.github.com>
2017-12-20 15:08:20 +0900

    na chalnari aat
    ghatli

--- pressuresensorwithairvalve/pressuresensorwithairvalve.ino
+++ pressuresensorwithairvalve/pressuresensorwithairvalve.ino
@@ -13,26 +13,42 @@ void loop() {
   leftforce = analogRead(leftpressurePin);
   Serial.println(rightforce);
   Serial.println(leftforce);
-if(leftforce > 500)
-{
-  Serial.println("left mid stance is right swing");
-  digitalWrite(6,HIGH);
-}
-else
-{
-  Serial.println("left mid stance finish");
-  digitalWrite(6,LOW);
-}
-if(rightforce > 700)
-{
-  Serial.println("right mid stance is left swing");
-  digitalWrite(3,HIGH);
-}
-else
-{
-    Serial.println("right mid stance finish");
-    digitalWrite(3,LOW);
-}
-delay(1);
+  /*
+   * Below if conditions detects the stance phase and support swing phase of contralateral limb.
+   * i.e. mid stance on rigt limb will actuates pgm on left limb to support and assist swing phase. 
+   */
+  if(leftforce > 500)
+  {
+    Serial.println("left mid stance is right swing");
+    digitalWrite(6,HIGH);
+  }
+  else
+  {
+    Serial.println("left mid stance finish");
+    digitalWrite(6,LOW);
+  }
+  if(rightforce > 700)
+  {
+    Serial.println("right mid stance is left swing");
+    digitalWrite(3,HIGH);
+  }
+  else
+  {
+      Serial.println("right mid stance finish");
+      digitalWrite(3,LOW);
+  }
+  /*
+   * Below code tries to detect no walking motion to void actuating pgm in such situation due gait 
+   * detection code. e.g. sitting, standing or anytihng that results in not walking actuation of 
+   * pgm is avoided. We try to do that by our first assumption i.e.
+   * 1. When not walking both the sensors will return values greater than threshold. 
+   */
+   if(rightforce >500 && left force > 450)
+   {
+    Serial.println("user not moving")
+    digitalWrite(3,LOW)
+    digitalWrite(6,LOW)
+   }
+  delay(1);
 }
 
